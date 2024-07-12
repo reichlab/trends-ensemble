55b118c9dc7e82c3519b06a9e594a82da2cbbf51
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-07-12 16:04:48 -0400

    Create functions to validate target data and model variations

---
+++ R/validate_model_inputs.R
@@ -0,0 +1,57 @@
+#' Perform simple validations on the model variations dataframe used to define a
+#' given baseline model
+#'
+#' @param model_variations a `data.frame` where each row specifies a set of
+#'   hyperparameters to use for a single baseline model fit, with the following
+#'   columns: `transformation`, `symmetrize`, and `window_size`. See the details
+#'   for more information.
+#' @details The types, and possible values for each of the columns in
+#' `model_variations` are as follows:
+#'   - transformation (character): "none" or "sqrt"
+#'   - symmetric (boolean)
+#'   - window_size (integer)
+#'
+#' @return no return value
+#'
+#' @noRd
+
+validate_model_variations <- function(model_variations) {
+  variation_col <- c("transformation", "symmetrize", "window_size")
+  actual_col <- colnames(model_variations)
+  if (!all(variation_col %in% actual_col)) {
+    cli::cli_abort("{.arg model_variations} is missing the column{?s}: {.val {setdiff(variation_col, actual_col)}}.")
+  } else if (!all(actual_col %in% variation_col) && all(variation_col %in% actual_col)) {
+    cli::cli_warn("{.arg model_variations} contains the extra column{?s}: {.val {setdiff(actual_col, variation_col)}}.")
+  }
+
+  valid_transformations <- c("none", "sqrt")
+  if (all(unique(model_variations$transformation) %in% valid_transformations)) {
+    cli::cli_abort("{.arg symmetrize} must contain only values {.val valid_transformations}")
+  }
+  if (!inherits(model_variations$symmetrize, "logical")) {
+    cli::cli_abort("{.arg symmetrize} must contain only logical values, e.g. TRUE or FALSE.")
+  }
+  if (!inherits(model_variations$window_size, "integer")) {
+    cli::cli_abort("{.arg window_size} must contain only integer values.")
+  }
+}
+
+
+#' Perform simple validations on the target data (time series) dataframe
+#'
+#' @param target_ts a `data.frame` of target data in a time series format
+#'   (contains columns `time_index`, `location`, and `observation`)
+#'
+#' @return no return value
+#'
+#' @noRd
+validate_target_ts <- function(target_ts) {
+  target_col <- c("time_index", "location", "observation")
+  actual_col <- colnames(target_ts)
+  if (!all(target_col %in% actual_col)) {
+    cli::cli_abort("{.arg target_ts} is missing the column{?s}: {.val {setdiff(target_col, actual_col)}}.")
+  } else if (!all(actual_col %in% target_col) && all(target_col %in% actual_col)) {
+    cli::cli_abort(c(x = "{.arg target_ts} contains the extra column{?s}: {.val {setdiff(actual_col, target_col)}}.",
+                     i = "Double check that your target data does not contain duplicate rows when removing columns."))
+  }
+}
